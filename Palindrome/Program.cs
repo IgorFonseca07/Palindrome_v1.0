2c1cf1b5cb9ea7f847367c9d387cb053d553700c
Igor Oliveira Fonseca <igoroliveira0717@gmail.com>
2020-06-25 22:45:10 -0300

    Pair - Done

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,34 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string sequenceCharacters = Console.ReadLine().ToUpper().Trim();
+
+            if (sequenceCharacters.Length % 2 == 0)
+            {
+                int count = 0;
+                for (int i = 0; i < (sequenceCharacters.Length / 2); i++)
+                {
+                    if (sequenceCharacters[i] == sequenceCharacters[(sequenceCharacters.Length) - i])
+                    {
+                        count++;
+                    }
+                }
+                if (count == sequenceCharacters.Length / 2)
+                {
+                    Console.WriteLine("It's Palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine("It's not Palindrome!");
+                }
+            }
+
+
+            Console.WriteLine(quantityCharacteres);
+            Console.WriteLine(sequenceCharacters);
+
+
         }
     }
 }
